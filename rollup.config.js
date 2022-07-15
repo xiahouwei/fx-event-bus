900343f171bb1c49eeeb936f941454464ff7e3c7
商华伟 <xiahouwei@aliyun.com>
2022-07-15 11:47:13 +0800

    优化readme

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,13 @@
 import { terser } from 'rollup-plugin-terser'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-// import babel from "@rollup/plugin-babel"
 export default {
 	input: 'src/index.js',
 	output: [{
-		file: './dist/eventbus.min.js',
+		file: './eventbus.min.js',
 		format: 'cjs',
 		name:'eventbusjs',
 		exports: 'auto'
-	}, {
-		file: './dist/eventbus.ems.js',
-		format: "esm",
-		name:'eventbusjs',
-		exports: 'auto'
-	},],
+	}],
 	plugins: [
-		terser(),
-		resolve(),
-		commonjs(),
-		// babel({ babelHelpers: 'bundled' })
+		terser()
 	]
 };
